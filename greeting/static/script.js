95c61f9603346852444c516669bca3b60503f724
Lekshmy VP <lekshmyprakash777@gmail.com>
2026-09-11 09:43:03 +0530

    My Task Project

---
+++ greeting/static/script.js
@@ -0,0 +1,5 @@
+function onClick() {
+    alert("Hello, welcome to my first Django app!");
+    document.getElementsByTagName('h1')[0].innerHTML = "welcome to my first";
+    document.getElementById('content').innerHTML = "welcome to my first Django app!";
+}
